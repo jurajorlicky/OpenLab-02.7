1a02e6a9b4251f555ccdd77e6145fed5aa7067c3
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:33:08 +0100

    Add project files.

---
+++ OpenLab 02.7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OpenLab_02._7
+{
+    class Program
+    {
+        public static bool vianoce(int den, int mesiac, int rok)
+        {
+            if (den == 24 && mesiac == 12)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadaj deň");
+            int den = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadaj mesiac");
+            int mesiac = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadaj rok");
+            int rok = int.Parse(Console.ReadLine());
+            Console.WriteLine(vianoce(den, mesiac, rok));
+        }
+    }
+}
